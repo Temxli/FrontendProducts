5c12207224be5990639e5eb0c648f20fea134410
Temxli <dayron4ik1@gmail.com>
2025-10-23 14:15:39 +0500

    layout merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Header from "./components/header";
+import Header from "./components/Header/Header";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
